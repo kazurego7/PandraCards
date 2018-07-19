12591fcaf44ae6922dc4259665efdbec8e3a8ac2
kazurego7 <kazurego7@gmail.com>
2018-07-20 07:57:17 +0900

    ・Shufflerクラスを削除し、ViewCardShuffle関数は、Deckクラスへ 理由：カード個々の動きは、カードでなくデッキが統率すべきなので

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -17,15 +17,29 @@ public class Deck : MonoBehaviour {
 	readonly String pathForCards = "Prefabs/Cards/";
 	IList<GameObject> deck;
 
+	public IEnumerator ViewCardShuffle (GameObject card) {
+		var moveingFrame = 20;
+		var start = transform.position;
+		var moveVec = card.transform.right * 3f;
+		var middle = start + moveVec;
+		foreach (var frame in Enumerable.Range (1, moveingFrame)) {
+			card.transform.position = Vector3.Lerp (start, middle, (float) frame / moveingFrame);
+			yield return null;
+		}
+		foreach (var frame in Enumerable.Range (1, moveingFrame)) {
+			card.transform.position = Vector3.Lerp (middle, start, (float) frame / moveingFrame);
+			yield return null;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 		DeckInit ();
+		StartCoroutine (ViewDeckShuffle ());
 	}
 
 	// Update is called once per frame
-	void Update () {
-
-	}
+	void Update () { }
 	void DeckInit () {
 		var cardRacipes = new List<CardRacipe> () {
 			new CardRacipe ("RedCard", 10),
@@ -54,11 +68,18 @@ public class Deck : MonoBehaviour {
 	}
 	public GameObject TopDraw () {
 		if (this.IsNoCard ()) {
-			return Instantiate (Resources.Load<GameObject> (pathForCards + "EmptyCard"), transform.position, Quaternion.identity, transform);
+			var emptyCard = Resources.Load<GameObject> (pathForCards + "EmptyCard");
+			return Instantiate (emptyCard, transform.position, Quaternion.identity, transform);
 		} else {
 			var top = deck[0];
 			deck.RemoveAt (0);
 			return top;
+		}
+	}
+	public IEnumerator ViewDeckShuffle () {
+		foreach (var card in deck) {
+			StartCoroutine (ViewCardShuffle (card));
+			yield return null;
 		}
 	}
 }
