e595de1ad79c14d92aace5aca3d33cd975a41fcd
kazurego7 <kazurego7@gmail.com>
2018-07-23 22:28:48 +0900

    HandSelector追加。 後々、手札選択のとき、ほかの手札の選択状態によって挙動を変えられる。

---
+++ Assets/HandSelected.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSelected : MonoBehaviour {
+	public bool IsSelected {
+		set;
+		get;
+	}
+	HandSelector handSelector;
+	GameObject selectionFrame;
+	void Start () {
+		handSelector = transform.GetComponentInParent<HandSelector> ();
+		selectionFrame = transform.GetChild (0).gameObject;
+		IsSelected = selectionFrame.activeInHierarchy;
+		//Debug.Log (selectionFrame);
+	}
+
+	public void Select () {
+		if (IsSelected) {
+			IsSelected = false;
+			DrawSelected ();
+		} else {
+			IsSelected = true;
+			handSelector.UpdateSelected (this);
+		}
+	}
+
+	public void DrawSelected () {
+		selectionFrame.SetActive (IsSelected);
+	}
+}
---
+++ Assets/HandSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HandSelector : MonoBehaviour {
+	IList<HandSelected> handSelecteds;
+	void Start () {
+		handSelecteds = GetComponentsInChildren<HandSelected> ().OrderBy ((t => t.name)).ToList ();
+	}
+	public void UpdateSelected (HandSelected updated) {
+		var predicate = true;
+		if (predicate) {
+			updated.DrawSelected ();
+		} else {
+			foreach (var selected in handSelecteds) {
+				selected.IsSelected = false;
+				selected.DrawSelected ();
+			}
+		}
+	}
+}
