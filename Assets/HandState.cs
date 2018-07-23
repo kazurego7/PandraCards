a31c1fc1c56a526ffdae64d1b8848565a817f2e7
kazurego7 <kazurego7@gmail.com>
2018-07-23 15:01:36 +0900

    カード選択の描画追加

--- Assets/HandState.cs
+++ Assets/HandState.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HandState : MonoBehaviour {
-	public bool IsSelected {
+	bool IsSelected {
 		set;
 		get;
+	}
+
+	GameObject selectionFrame;
+	void Start () {
+		selectionFrame = transform.GetChild (0).gameObject;
+		Debug.Log (selectionFrame);
 	}
 
 	public void ChangeSelect () {
 		IsSelected = !IsSelected;
 		Debug.Log (this.name + ":" + IsSelected.ToString ());
+		DrawSelect ();
 	}
 
 	public void DrawSelect () {
-		if (IsSelected) {
-			// UI表示
-		} else {
-			// UI非表示
-		}
+		selectionFrame.SetActive (IsSelected);
 	}
 }
