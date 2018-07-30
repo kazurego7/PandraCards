e036fffdf83f96bee1f5f60928689d874e4d7460
kazurego7 <kazurego7@gmail.com>
2018-07-30 16:55:57 +0900

    UniRxの導入と、試し

---
+++ Assets/Scripts/ShuffleDrawer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ShuffleDrawer : MonoBehaviour {
+    IObservable<Vector3> shufflePipe;
+    [SerializeField] int moveingFrame = 100;
+    void Awake () {
+        // 定義
+        var moveVec = transform.right * 3f;
+        var start = transform.position;
+        var middle = start + moveVec;
+        var goShuffle = Observable
+            .IntervalFrame (1)
+            .Take (moveingFrame)
+            .Select (frame => Vector3.Lerp (start, middle, (float) frame / moveingFrame));
+
+        var comebackShuffle = Observable
+            .IntervalFrame (1)
+            .Take (moveingFrame)
+            .Select (frame => Vector3.Lerp (middle, start, (float) frame / moveingFrame));
+
+        shufflePipe = goShuffle.Concat (comebackShuffle);
+    }
+
+    public void DrawKthCardShuffle () {
+        shufflePipe
+            .Subscribe (pos => transform.position = pos)
+            .AddTo (this);
+    }
+}
