53eaa1663138ae92bfcb52ce9b89705eda6fc4ec
kazurego7 <kazurego7@gmail.com>
2018-08-30 10:47:39 +0900

    CardPutPresenterの各ReactiveCollectionのListから、一つのAddストリームとRemoveストリームにまとめる

--- Assets/Scripts/CardPutPresenter.cs
+++ Assets/Scripts/CardPutPresenter.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using System;
+using System.Linq;
 
 public class CardPutPresenter {
 
-	ReactiveCollection<IList<Card>> PlayCardsNotice {
+	IList<IReactiveCollection<IList<Card>>> PlayCardsNotices {
 		get;
 		set;
-	}
+	} = new List<IReactiveCollection<IList<Card>>>();
 
-	ReactiveCollection<Card> DeckNotice {
+	IList<IReactiveCollection<Card>> DeckCardNotices {
 		get;
 		set;
-	}
+	} = new List<IReactiveCollection<Card>>();
 
-	ReactiveCollection<IList<Card>> HandCardsNotice {
+	IList<IReactiveCollection<IList<Card>>> HandCardsNotices {
 		get;
 		set;
-	}
+	} = new List<IReactiveCollection<IList<Card>>>();
 
 	public ReactiveProperty<Vector3> Position {
 		get;
-	} = new ReactiveProperty<Vector3>(
-		Observable.
-	)
+	} = new ReactiveProperty<Vector3>();
+
+	CardPutPresenter() {
+		(IObservable<T> addObservable, IObservable<T> removeObservable) RxCollections2AddAndRemoveObservables<T> (IList<IReactiveCollection<T>> rxCollections) {
+			return rxCollections
+			.Select(notice => (notice.ObserveAdd().Select(x => x.Value), notice.ObserveRemove().Select(x => x.Value))).Aggregate((accm, x) => (accm.Item1.Merge(x.Item1), accm.Item2.Merge(x.Item2)));
+		}
+		var deckAddAndRemoveObservable = RxCollections2AddAndRemoveObservables(DeckCardNotices);
+		var handAddAndRemoveObservable = RxCollections2AddAndRemoveObservables(HandCardsNotices);
+		var playCardsAddAndRemoveObservable = RxCollections2AddAndRemoveObservables(PlayCardsNotices);
+	}
 }
