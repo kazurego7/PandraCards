370528e3745de9762f5a5e9e25a787cba63423bd
kazurego7 <kazurego7@gmail.com>
2018-09-29 16:39:24 +0900

    UniRxアプデ

---
+++ Assets/Scripts/Presenter/CardPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public class CardPresenter : MonoBehaviour {
+
+	static readonly DeckPresenter deckPresenter;
+	static readonly OneHandPresenter[] oneHandPresenters;
+	static readonly PlayAreaPresenter[] playAreaPresenters;
+	static readonly DiscardBoxPresenter discardBoxPresenter;
+}
--- Assets/Scripts/Presenter/Store.cs
+++ Assets/Scripts/Presenter/Store.cs
@@ -1,19 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UniRx;
 using UnityEngine;
 
-public class Store : MonoBehaviour {
-	public struct OneHandStore {
-		public OneHand model;
-		public Transform transform;
+public class Store {
+	public readonly (OneHandStore[], DeckStore) yourResouce;
+	public readonly (OneHandStore[], DeckStore) opponentResouce;
+	public readonly (PlayAreaStore, PlayAreaStore) playAreas;
+	public readonly DiscardBoxStore discardBox;
+	public readonly CardStore[] cards;
+
+	public abstract class Message {
+		public class Replenished : Message {
+			public readonly (Transform source, Transform target) msg;
+		}
+		public class Played : Message {
+			public readonly (Transform source, Transform target) msg;
+		}
 	}
 
-	public struct CardStore {
-		public Card model;
-		public Transform transform;
+	public Store () {
+
 	}
 
-	public struct HandStore {
-		public OneHandStore[] handStores;
+	public readonly struct OneHandStore {
+		public readonly Transform transform;
+		public readonly OneHand onehand;
+	}
+	public readonly struct DeckStore {
+		public readonly Transform transform;
+		public readonly Deck deck;
+	}
+	public readonly struct PlayAreaStore {
+		public readonly PlayArea playArea;
+		public readonly Transform transform;
+	}
+
+	public readonly struct DiscardBoxStore {
+		public readonly DiscardsBox discardBox;
+		public readonly Transform transform;
+	}
+	public readonly struct CardStore {
+		public readonly Card card;
+		public readonly Transform transform;
 	}
 }
