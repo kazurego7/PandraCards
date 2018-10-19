5c02b10c61178c58d2ddc084d51f96471753d041
kazurego7 <kazurego7@gmail.com>
2018-10-19 10:40:13 +0900

    add view procedure
    各viewの処理を追加した。（未テスト）

--- Assets/Scripts/Presenter/Store.cs
+++ Assets/Scripts/Presenter/Store.cs
@@ -7,64 +7,96 @@ using UnityEngine;
 
 public class Store {
 
-	IReadOnlyDictionary<OneHand, OneHandView> MyHandPresenter { get; }
-		(Deck model, DeckView view) MyDeckPresenter { get; }
-	IReadOnlyDictionary<PlayArea, PlayAreaView> PlayAreaPresenter { get; }
-		(DiscardsBox model, DiscardBoxView view) DiscardBoxPresenter { get; }
-	IReadOnlyDictionary<Card, CardView> CardPresenter { get; }
+	public IReadOnlyDictionary<OneHand, OneHandView> MyHandPresenter { get; }
+	public (Deck model, DeckView view) MyDeckPresenter { get; }
+	public IReadOnlyDictionary<PlayArea, PlayAreaView> PlayAreaPresenter { get; }
+	public (DiscardsBox model, DiscardBoxView view) DiscardBoxPresenter { get; }
+	public IReadOnlyDictionary<Card, CardView> CardPresenter { get; }
 
 	public Store () {
 		var replenishMessage = Observable.Merge (MyHandPresenter.Keys.Select (model => model.ReplenishedNotice));
-		var replenishDraw = replenishMessage
-			.Select (msg => {
-				var cardView = CardPresenter[msg.mover];
-				var oneHandView = MyHandPresenter[msg.target];
-				return OneHandView.Replenish (cardView, oneHandView);
-			});
+		var replenishDraw = replenishMessage.Select (msg => {
+			var cardView = CardPresenter[msg.mover];
+			var oneHandView = MyHandPresenter[msg.target];
+			return OneHandView.Replenish (cardView, oneHandView);
+		});
 		var shuffleMessage = MyDeckPresenter.model.ShuffledNotice;
-		var shuffleDraw = shuffleMessage
-			.Select (msg => {
-				var cardViews = msg.Select (card => CardPresenter[card]).ToList ();
-				return DeckView.Shuffle (cardViews);
-			});
+		var shuffleDraw = shuffleMessage.Select (msg => {
+			var cardViews = msg.Select (card => CardPresenter[card]).ToList ();
+			return DeckView.Shuffle (cardViews);
+		});
 		var playMessage = Observable.Merge (PlayAreaPresenter.Keys.Select (model => model.PlayNotice));
-		var playDraw = playMessage
-			.Select (msg => {
-				var cardViews = msg.playCards.Select (card => CardPresenter[card]).ToList ();
-				var playAreaView = PlayAreaPresenter[msg.putArea];
-				return PlayAreaView.Play (cardViews, playAreaView);
-			});
+		var playDraw = playMessage.Select (msg => {
+			var cardViews = msg.playCards.Select (card => CardPresenter[card]).ToList ();
+			var playAreaInfo = (msg.putArea, PlayAreaPresenter[msg.putArea]);
+			return PlayAreaView.Play (cardViews, playAreaInfo);
+		});
 		var firstPutMessage = Observable.Merge (PlayAreaPresenter.Keys.Select (model => model.FirstPutNotice));
-		var firstPutDraw = firstPutMessage
-			.Select (msg => {
-				var cardView = CardPresenter[msg.firstCard];
-				var playAreaView = PlayAreaPresenter[msg.putArea];
-				return PlayAreaView.FirstPut (cardView, playAreaView);
-			});
+		var firstPutDraw = firstPutMessage.Select (msg => {
+			var cardView = CardPresenter[msg.firstCard];
+			var playAreaView = PlayAreaPresenter[msg.putArea];
+			return PlayAreaView.FirstPut (cardView, playAreaView);
+		});
 		var discardMessage = DiscardBoxPresenter.model.DiscardNotice;
-		var discardDraw = discardMessage
-			.Select (msg => {
-				var cardViews = msg.discards.SelectMany (cards => cards.Select (card => CardPresenter[card])).ToList ();
-				var discardBoxView = DiscardBoxPresenter.view;
-				return DiscardBoxView.Discard (cardViews, discardBoxView);
-			});
+		var discardDraw = discardMessage.Select (msg => {
+			var cardViews = msg.discards.SelectMany (cards => cards.Select (card => CardPresenter[card])).ToList ();
+			var discardBoxView = DiscardBoxPresenter.view;
+			return DiscardBoxView.Discard (cardViews, discardBoxView);
+		});
+
 	}
 
 	public struct OneHandView {
 		public static IObservable<Unit> Replenish (CardView cardView, OneHandView oneHandView) {
-			return Observable.ReturnUnit ();
+			var nextPoss = CardView.LinerMoves (source: cardView.Transform.position, target: oneHandView.Transform.position, moveingFrame: 7);
+			return nextPoss.Select (nextPos => {
+				cardView.Transform.Rotate (nextPos);
+				return Unit.Default;
+			});
 		}
 		public Transform Transform { get; }
 	}
 	public struct DeckView {
 		public static IObservable<Unit> Shuffle (IList<CardView> cards) {
-			return Observable.ReturnUnit ();
+			var delayFrame = 1;
+			return Observable
+				.TimerFrame (0, delayFrame)
+				.TakeWhile (t => t <= cards.Count)
+				.SelectMany (countByTop => ShuffleOne (cards[(int) countByTop]));
+			IObservable<Unit> ShuffleOne (CardView cardView) {
+				// 各カード1枚ごとの処理
+				var moveVec = cardView.Transform.right * 3f;
+				var start = cardView.Transform.position;
+				var middle = start + moveVec;
+
+				var movePos = CardView.LinerMoves (start, middle, moveingFrame : 10)
+					.Concat (CardView.LinerMoves (middle, start, moveingFrame : 10));
+
+				return movePos.Select (nextPos => {
+					cardView.Transform.Rotate (nextPos);
+					return Unit.Default;
+				});
+			}
 		}
 		public Transform Transform { get; }
 	}
 	public struct PlayAreaView {
-		public static IObservable<Unit> Play (IList<CardView> cardViews, PlayAreaView playAreaView) {
-			return Observable.ReturnUnit ();
+		public static IObservable<Unit> Play (IList<CardView> cardViews, (PlayArea model, PlayAreaView view) playAreaInfo) {
+			var playCardBottomPosZ = (playAreaInfo.model.CountPlayedCards () - cardViews.Count) * Card.thickness * Vector3.back;
+			return cardViews
+				.Select ((cardView, i) => {
+					var distance = 0.2f;
+					var moveXY = distance * (-(cardViews.Count - 1) + 2 * i) * Vector3.left;
+					var moveZ = playCardBottomPosZ + Card.thickness * i * Vector3.back;
+					var source = cardView.Transform.position;
+					var cardMove = CardView.LinerMoves (source, source + moveZ)
+						.Concat (CardView.LinerMoves (source + moveZ, source + moveZ + moveXY));
+					return cardMove.Select (nextPos => {
+						cardView.Transform.Rotate (nextPos);
+						return Unit.Default;
+					});
+				})
+				.Merge ();
 		}
 		public static IObservable<Unit> FirstPut (CardView cardView, PlayAreaView playAreaView) {
 			return Observable.ReturnUnit ();
@@ -79,6 +111,12 @@ public class Store {
 		public Transform Transform { get; }
 	}
 	public struct CardView {
+		public static IObservable<Vector3> LinerMoves (Vector3 source, Vector3 target, int moveingFrame = 1) {
+			return Observable
+				.TimerFrame (0, 1)
+				.TakeWhile (t => t <= moveingFrame)
+				.Select (t => Vector3.Lerp (source, target, (float) t / moveingFrame));
+		}
 		public Transform Transform { get; }
 	}
 }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test {
+	interface IType { }
+	class TypeFactory {
+		IType Create (int i) {
+			if (i == 1) {
+				return new Type1 (i, 'c');
+			} else {
+				return new Type2 (true);
+			}
+		}
+	}
+	class Type1 : IType {
+		public Type1 (int i, char c) {
+			_i = i;
+			_c = c;
+		}
+		int _i;
+		char _c;
+	}
+	class Type2 : IType {
+		public Type2 (bool b) {
+			_b = b;
+		}
+		bool _b;
+	}
+}
